b08eef611a382c90220aff01a7483fc8613abf2b
Ramiro Suarez <suarezramirof@gmail.com>
2023-01-20 13:26:27 -0300

    FIX apiProductos (la propia clase maneja la persistencia en el json), FIX rutasProductos error handling

--- src/api/apiProductos.js
+++ src/api/apiProductos.js
@@ -1,19 +1,29 @@
+import fs from "fs";
+
 class Productos {
-  #productos;
-  constructor(productos) {
-    this.#productos = productos;
+  async updateProductos(productos) {
+    try {
+      await fs.promises.writeFile(
+        "./data/productos.json",
+        JSON.stringify(productos),
+        "utf-8"
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 
-  initialize(productos) {
-    this.#productos = productos;
+  async getAll() {
+    const file = await fs.promises
+      .readFile("./data/productos.json")
+      .then((data) => JSON.parse(data));
+    return file;
   }
 
-  getAll() {
-    return this.#productos;
-  }
-
-  getItemById = (id) => {
-    const [item] = this.#productos.filter((elem) => elem.id == id);
+  async getItemById(id) {
+    const [item] = await this.getAll().then((items) =>
+      items.filter((elem) => elem.id == id)
+    );
     if (item) return item;
     else if (isNaN(id)) {
       const error = new Error("El id ingresado no es válido");
@@ -24,9 +34,9 @@ class Productos {
       error.code = 404;
       throw error;
     }
-  };
+  }
 
-  addItem = (producto) => {
+  async addItem(producto) {
     if (
       producto.nombre &&
       producto.descripcion &&
@@ -36,63 +46,83 @@ class Productos {
       producto.stock
     ) {
       let idMax = 0;
-      for (let item of this.#productos) {
-        if (item.id > idMax) idMax = item.id;
-      }
-      const newId = idMax + 1;
-      this.#productos.push({ ...producto, id: newId, timestamp: Date.now() });
+      await this.getAll().then((productos) => {
+        for (let item of productos) {
+          if (item.id > idMax) idMax = item.id;
+        }
+        const newId = idMax + 1;
+        const item = { ...producto, id: newId, timestamp: Date.now() };
+        productos.push(item);
+        return this.updateProductos(productos);
+      });
     } else {
       const error = new Error("Ingrese todos los datos del producto");
       error.code = 400;
       throw error;
     }
-  };
+  }
 
-  updateItem = (id, { nombre, descripcion, codigo, foto, precio, stock }) => {
+  async updateItem(id, { nombre, descripcion, codigo, foto, precio, stock }) {
+    if (isNaN(id)) {
+      const error = new Error("Ingrese un id válido");
+      error.code = 400;
+      throw error;
+    } else if (!(nombre || descripcion || codigo || foto || precio || stock)) {
+      const error = new Error("Ingrese alguna propiedad válida");
+      error.code = 400;
+      throw error;
+    } else {
+      await this.getAll().then((productos) => {
+        let idValido = false;
+        for (let item of productos) {
+          if (item.id == id) idValido = true;
+        }
+        if (!idValido) {
+          const error = new Error("Producto no encontrado");
+          error.code = 404;
+          throw error;
+        } else {
+          productos = productos.map((item) =>
+            item.id == id
+              ? {
+                  ...item,
+                  nombre: nombre ? nombre : item.nombre,
+                  descripcion: descripcion ? descripcion : item.descripcion,
+                  codigo: codigo ? codigo : item.codigo,
+                  foto: foto ? foto : item.foto,
+                  precio: precio ? precio : item.precio,
+                  stock: stock ? stock : item.stock,
+                }
+              : item
+          );
+          return this.updateProductos(productos);
+        }
+      });
+    }
+  }
+
+  async deleteItemById(id) {
     if (isNaN(id)) {
       const error = new Error("Ingrese un id válido");
       error.code = 400;
       throw error;
     } else {
-      let idValido = false;
-      for (let item of this.#productos) {
-        if (item.id == id) idValido = true;
-      }
-      if (!idValido) {
-        const error = new Error("Producto no encontrado");
-        error.code = 404;
-        throw error;
-      } else {
-        this.#productos = this.#productos.map((item) =>
-          item.id == id
-            ? {
-                nombre: nombre ? nombre : item.nombre,
-                descripcion: descripcion ? descripcion : item.descripcion,
-                codigo: codigo ? codigo : item.codigo,
-                foto: foto ? foto : item.foto,
-                precio: precio ? precio : item.precio,
-                stock: stock ? stock : item.stock,
-                id,
-              }
-            : item
-        );
-      }
+      await this.getAll().then((productos) => {
+        let idValido = false;
+        for (let item of productos) {
+          if (item.id == id) idValido = true;
+        }
+        if (!idValido) {
+          const error = new Error("Producto no encontrado");
+          error.code = 404;
+          throw error;
+        } else {
+          productos = productos.filter((item) => item.id != id);
+          return this.updateProductos(productos);
+        }
+      });
     }
-  };
-
-  deleteItemById = (id) => {
-    if (isNaN(id)) {
-      const error = new Error("Ingrese un id válido");
-      error.code = 400;
-      throw error;
-    } else if (this.#productos.map((item) => item.id).includes(parseInt(id))) {
-      this.#productos = this.#productos.filter((item) => item.id != id);
-    } else {
-      const error = new Error("Producto no encontrado");
-      error.code = 404;
-      throw error;
-    }
-  };
+  }
 }
 
 export default Productos;
--- src/routes/rutasProductos.js
+++ src/routes/rutasProductos.js
@@ -1,95 +1,101 @@
 import express from "express";
 import { admin } from "../../server.js";
 import Productos from "../api/apiProductos.js";
-import MemoriaProductos from "../data/persistenciaProductos.js";
 
 const productos = new Productos([]);
-const persistencia = new MemoriaProductos();
 
 export const productosRouter = express.Router();
 
-persistencia
-  .leerProductos()
-  .then((data) => {
-    productos.initialize(data);
-  })
-  .catch((error) => console.log(error));
-
 productosRouter.get("/", (req, res) => {
-    res.header({ "Access-Control-Allow-Origin": "*" });
-    res.json(productos.getAll());
-  });
-  
-  productosRouter.get("/:id", (req, res) => {
-    try {
-      const { id } = req.params;
-      res.json(productos.getItemById(id));
-    } catch (error) {
+  res.header({ "Access-Control-Allow-Origin": "*" });
+  productos
+    .getAll()
+    .then((data) => res.json(data))
+    .catch((error) => {
       res.status(error.code ? error.code : 500).json({ error: error.message });
-    }
-  });
-  
-  productosRouter.post("/", (req, res) => {
-    if (!admin) {
-      res.status = 401;
-      res.json({
-        error: -1,
-        descripcion: `Ruta ${req.originalUrl} con método ${req.method} no autorizada`,
-      });
-    } else {
-      try {
-        const producto = req.body;
-        productos.addItem(producto);
-        persistencia.updateProductos(productos.getAll());
+    });
+});
+
+productosRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  productos
+    .getItemById(id)
+    .then((item) => res.json(item))
+    .catch((error) =>
+      res.status(error.code ? error.code : 500).json({ error: error.message })
+    );
+});
+
+productosRouter.post("/", (req, res) => {
+  if (!admin) {
+    res.status = 401;
+    res.json({
+      error: -1,
+      descripcion: `Ruta ${req.originalUrl} con método ${req.method} no autorizada`,
+    });
+  } else {
+    const producto = req.body;
+    productos
+      .addItem(producto)
+      .then(() => {
         res.json({ successMessage: "Carga exitosa" });
-      } catch (error) {
-        res.status(error.code ? error.code : 500).json({ error: error.message });
-      }
-    }
-  });
-  
-  productosRouter.put("/:id", (req, res) => {
-    if (!admin) {
-      res.status = 401;
-      res.json({
-        error: -1,
-        descripcion: `Ruta ${req.originalUrl} con método ${req.method} no autorizada`,
+      })
+      .catch((error) => {
+        res
+          .status(error.code ? error.code : 500)
+          .json({ error: error.message });
       });
-    } else {
-      try {
-        const { nombre, descripcion, codigo, foto, stock, precio } = req.body;
-        const id = req.params.id;
-        productos.updateItem(id, {
-          nombre,
-          descripcion,
-          codigo,
-          foto,
-          stock,
-          precio,
-        });
-        persistencia.updateProductos(productos.getAll());
+  }
+});
+
+productosRouter.put("/:id", (req, res) => {
+  if (!admin) {
+    res.status = 401;
+    res.json({
+      error: -1,
+      descripcion: `Ruta ${req.originalUrl} con método ${req.method} no autorizada`,
+    });
+  } else {
+    const { nombre, descripcion, codigo, foto, stock, precio } = req.body;
+    const id = req.params.id;
+    productos
+      .updateItem(id, {
+        nombre,
+        descripcion,
+        codigo,
+        foto,
+        stock,
+        precio,
+      })
+      .then(() => {
         res.json({ successMessage: "Actualización exitosa" });
-      } catch (error) {
-        res.status(error.code ? error.code : 500).json({ error: error.message });
-      }
-    }
-  });
-  
-  productosRouter.delete("/:id", (req, res) => {
-    if (!admin) {
-      res.status = 401;
-      res.json({
-        error: -1,
-        descripcion: `Ruta ${req.originalUrl} con método ${req.method} no autorizada`,
+      })
+      .catch((error) => {
+        res
+          .status(error.code ? error.code : 500)
+          .json({ error: error.message });
       });
-    } else {
-      try {
-        const id = req.params.id;
-        productos.deleteItemById(id);
-        persistencia.updateProductos(productos.getAll());
+  }
+});
+
+productosRouter.delete("/:id", (req, res) => {
+  if (!admin) {
+    res.status = 401;
+    res.json({
+      error: -1,
+      descripcion: `Ruta ${req.originalUrl} con método ${req.method} no autorizada`,
+    });
+  } else {
+    const id = req.params.id;
+    productos
+      .deleteItemById(id)
+      .then(() => {
         res.json({ successMessage: "Eliminación exitosa" });
-      } catch (error) {
-        res.status(error.code ? error.code : 500).json({ error: error.message });
-      }
-    }
-  });
+      })
+      .catch((error) => {
+        res
+          .status(error.code ? error.code : 500)
+          .json({ error: error.message });
+      });
+  }
+});
