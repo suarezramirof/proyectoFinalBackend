286449971611076b136bf6441a58207d343f77c8
Ramiro Suarez <suarezramirof@gmail.com>
2023-01-29 22:24:21 -0300

    ADD contenedorMongo

---
+++ src/container/ContenedorMongo.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { mongo } from "../config.js";
+
+class ContenedorMongo {
+  constructor(db, type, schema) {
+    this.ruta = mongo + db;
+    this.db = db;
+    this.connect(this.ruta);
+    this.items = mongoose.model(type, schema);
+  }
+
+  connect(ruta) {
+    mongoose.connect(ruta, {
+      useNewUrlParser: true,
+    });
+  }
+
+  async getAll() {
+    const items = this.items.find({});
+    return items;
+  }
+
+  async get(id) {
+    const [item] = await this.items.find({ _id: id });
+    return item;
+  }
+
+  async add(item) {
+    const promise = this.items.create(item) 
+    return await promise.then((elem) => elem._id);
+  }
+
+  async updateId(id, item) {
+    await this.items.updateOne({ _id: id }, { $set: item });
+  }
+
+  async delete(id) {
+    await this.items
+      .deleteOne({ _id: id })
+      .then(() => console.log("Eliminación exitosa"));
+  }
+}
+
+export default ContenedorMongo;
