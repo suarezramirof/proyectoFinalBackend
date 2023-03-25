2461876a8e64ad9378af1c1e5fdf721cc43621c0
Ramiro Suarez <suarezramirof@gmail.com>
2023-03-25 11:04:42 -0300

    feat: mongoAtlasDB

--- src/config.js
+++ src/config.js
@@ -1,18 +1,38 @@
 import fs from "fs";
 import { config } from "dotenv";
 config();
+import parseArgs from "minimist";
 
-export const PORT = process.env.PORT || 8080
+const USER = process.env.USER_NAME;
+const PASSWORD = process.env.PASSWORD;
 
 export const MONGOPORT = "mongodb://127.0.0.1:27017/";
 export const MONGODB = "ecommerce";
+export const MongoAtlasUri = `mongodb+srv://${USER}:${PASSWORD}@cluster0.zuesfin.mongodb.net/${MONGODB}`;
+export const SessionTimeOut = 600000;
+const options = {
+  alias: {
+    p: "PORT",
+    m: "MODE",
+  },
+  default: {
+    PORT: process.env.PORT || 8080,
+    MODE: "FORK",
+  },
+};
 
-export const FBServiceAccount = JSON.parse(fs.readFileSync(
-  "./data/ecommerce-d09f4-firebase-adminsdk-78exg-9b039216fb.json",
-  "utf-8"
-)); 
+const commandLineArgs = process.argv.slice(2);
 
-export const DB = process.env.DB || "memory"
+export const { PORT, MODE } = parseArgs(commandLineArgs, options);
+
+export const FBServiceAccount = JSON.parse(
+  fs.readFileSync(
+    "./data/ecommerce-d09f4-firebase-adminsdk-78exg-9b039216fb.json",
+    "utf-8"
+  )
+);
+
+export const DB = process.env.DB || "memory";
 
 // Login
 
