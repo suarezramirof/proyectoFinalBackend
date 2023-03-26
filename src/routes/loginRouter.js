e25f18617012fe60f3597c2d612687025f6c736d
Ramiro Suarez <suarezramirof@gmail.com>
2023-03-26 17:59:57 -0300

    feat: add multer to accept files in register form

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -3,6 +3,23 @@ import passport from "passport";
 import { config } from "dotenv";
 config();
 const loginRouter = Router();
+import User from "../models/User.js";
+import registerMiddleware from "../auth/registerMiddleware.js";
+import { createHash } from "../auth/bCrypt.js";
+
+import multer from "multer";
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
 
 // loginRouter.get("/", (_req, res) => {
 //   res.redirect("/home");
@@ -12,14 +29,42 @@ loginRouter.get("/register", checkNotAuthenticated, (_req, res) => {
   res.render("pages/register");
 });
 
+// loginRouter.post(
+//   "/register",
+//   checkNotAuthenticated,
+//   passport.authenticate("register", {
+//     failureRedirect: "/registerfailure",
+//     successRedirect: "/login",
+//     session: false,
+//   })
+// );
+
 loginRouter.post(
   "/register",
   checkNotAuthenticated,
-  passport.authenticate("register", {
-    failureRedirect: "/registerfailure",
-    successRedirect: "/login",
-    session: false,
-  })
+  upload.single("file"),
+  registerMiddleware,
+  (req, res) => {
+    const userData = {
+      name: req.body.name,
+      address: req.body.address,
+      age: req.body.age,
+      phone: req.body.phone,
+      photo: req.file.path,
+    };
+    const newUser = new User({
+      email: req.body.username,
+      password: createHash(req.body.password),
+      userData: userData,
+    });
+    User.create(newUser, (err, userWithId) => {
+      if (err) {
+        console.log("Error when saving user: " + err);
+        return res.json(err);
+      }
+      return res.json(userWithId);
+    });
+  }
 );
 
 loginRouter.get("/registerfailure", checkNotAuthenticated, (_req, res) => {
@@ -55,11 +100,12 @@ const route = process.env.HOST == "LOCAL" ? true : false;
 // //   res.render("pages/home", { user: req.session.passport.user.username, ruta: route });
 // });
 
-loginRouter.get("/logout", checkAuthenticated, (req, res) => {
+loginRouter.get("/logout", checkAuthenticated, async (req, res) => {
   try {
-    const name = req.session.passport.user.username
-      ? req.session.passport.user.username
-      : "";
+    const { userData } = await User.findOne({
+      _id: req.session.passport.user.id,
+    });
+    const { name } = userData;
     req.session.destroy();
     res.render("pages/logout", { mensaje: `Hasta luego ${name}` });
   } catch (error) {
