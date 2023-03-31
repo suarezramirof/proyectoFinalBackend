941aa97b4635fa35e551e47788093c577892c4e8
Ramiro Suarez <suarezramirof@gmail.com>
2023-03-30 23:09:09 -0300

    refactor: registerMiddleware

--- src/auth/registerMiddleware.js
+++ src/auth/registerMiddleware.js
@@ -8,7 +8,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const registerMiddleware = (req, res, next) => {
-  console.log(req.body.username);
   User.findOne({ email: req.body.username }, (err, user) => {
     if (err) {
       console.log("Register error", err);
@@ -16,9 +15,8 @@ const registerMiddleware = (req, res, next) => {
     }
     if (user) {
       console.log("User already exists");
-      console.log(req.file.path);
       fs.unlinkSync(path.join(__dirname, "../../", req.file.path))
-      return res.redirect("/registerfailure");
+      return res.redirect("/register");
     }
     return next();
   });
